ca0317ea9d19a0945a9351744eb32903c88f6759
Axel Uzeta <axel.uzeta@indoff.com>
2025-10-23 13:18:12 -0700

    feat: setup cors and nvmrc for node >20 versions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { HealthModule } from './modules/health/health.module';
 import { CoursesModule } from './modules/courses/courses.module';
 
 @Module({
-  imports: [PrismaModule, HealthModule, CoursesModule],
+  imports: [
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    HealthModule,
+    CoursesModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = app.get(ConfigService);
+
+  const corsOrigins = config.get<string>('CORS_ORIGIN')?.split(',') || [];
+  app.enableCors({
+    origin: corsOrigins, // coincide EXACTO con el origin del front
+    credentials: true, // pon true solo si usarás cookies / credentials
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const port = config.get<number>('PORT') ?? 3000;
+  await app.listen(port);
+  console.log(`CORS origins: ${corsOrigins.join(', ') || 'none'}`);
 }
-bootstrap();
+void bootstrap();
